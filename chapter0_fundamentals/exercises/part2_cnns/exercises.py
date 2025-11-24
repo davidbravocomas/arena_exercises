a8d6e7c61db7b69a15343454ad8edb87a0ddce06
davidbravocomas <d.bravocomas@gmail.com>
2025-11-24 19:13:04 +0000

    Finished 0.2: 1

--- chapter0_fundamentals/exercises/part2_cnns/exercises.py
+++ chapter0_fundamentals/exercises/part2_cnns/exercises.py
@@ -39,5 +39,90 @@ class ReLU(nn.Module):
     def forward(self, x: Tensor) -> Tensor:
         return t.maximum(x, t.zeros(x.size()))
 
+# tests.test_relu(ReLU)
 
-tests.test_relu(ReLU)
+class Linear(nn.Module):
+    def __init__(self, in_features: int, out_features: int, bias=True):
+        """
+        A simple linear (technically, affine) transformation.
+
+        The fields should be named `weight` and `bias` for compatibility with PyTorch.
+        If `bias` is False, set `self.bias` to None.
+        """
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        bound = 1 / np.sqrt(in_features)
+        weight = bound * (2 * t.rand(out_features, in_features) - 1)
+        self.weight = nn.Parameter(weight)
+
+        if bias:
+            bias = bound * (2 * t.rand(out_features) - 1)
+            self.bias = nn.Parameter(bias)
+        else:
+            self.bias = None
+    
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        x: shape (*, in_features)
+        Return: shape (*, out_features)
+        """
+        x = einops.einsum(x, self.weight, "... in_feat, out_feat in_feat -> ... out_feat")
+        if self.bias is not None:
+            x += self.bias
+        return x
+        
+
+    def extra_repr(self) -> str:
+        print(f"Number of input features: {self.in_features}")
+        print(f"Number of output features: {self.out_features}")
+        print(f"bias={self.bias is not None}")
+
+
+# tests.test_linear_parameters(Linear, bias=False)
+# tests.test_linear_parameters(Linear, bias=True)
+# tests.test_linear_forward(Linear, bias=False)
+# tests.test_linear_forward(Linear, bias=True)
+
+class Flatten(nn.Module):
+    def __init__(self, start_dim: int = 1, end_dim: int = -1) -> None:
+        super().__init__()
+        self.start_dim = start_dim
+        self.end_dim = end_dim
+
+    def forward(self, input: Tensor) -> Tensor:
+        """
+        Flatten out dimensions from start_dim to end_dim, inclusive of both.
+        """
+        shape = input.shape
+
+        # Get start & end dims, handling negative indexing for end dim
+        start_dim = self.start_dim
+        end_dim = self.end_dim if self.end_dim >= 0 else len(shape) + self.end_dim
+
+        # Get the shapes to the left / right of flattened dims, as well as size of flattened middle
+        shape_left = shape[:start_dim]
+        shape_right = shape[end_dim + 1 :]
+        shape_middle = t.prod(t.tensor(shape[start_dim : end_dim + 1])).item()
+
+        return t.reshape(input, shape_left + (shape_middle,) + shape_right)
+
+    def extra_repr(self) -> str:
+        return ", ".join([f"{key}={getattr(self, key)}" for key in ["start_dim", "end_dim"]])
+
+class SimpleMLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = Flatten()
+        self.linear1 = Linear(in_features=28 * 28, out_features=100)
+        self.relu = ReLU()
+        self.linear2 = Linear(in_features=100, out_features=10)
+        
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.linear2(self.relu(self.linear1(self.flatten(x))))
+
+
+tests.test_mlp_module(SimpleMLP)
+tests.test_mlp_forward(SimpleMLP)
